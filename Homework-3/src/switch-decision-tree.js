41d8ed2bb5d02edae9da230898956228def9ce2a
KonstantynHertsev <gktsew@gmail.com>
2025-01-30 17:50:29 +0200

    Logical-operations

---
+++ Homework-3/src/switch-decision-tree.js
@@ -0,0 +1,17 @@
+function switchDecisionTree () {
+    const a1 = 1;
+    const a2 = 2;
+
+
+    switch (true) {
+        case a1 != a2:
+            console.log ('a1 not equal a2');
+            break;
+        default:
+            console.log ('a1 equals a2');
+            break;
+    }
+
+}
+
+switchDecisionTree ();
