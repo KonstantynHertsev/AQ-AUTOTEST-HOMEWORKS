0f6b3a67f76b0ee07abc719d54c109dd82fca2db
KonstantynHertsev <gktsew@gmail.com>
2025-02-02 22:00:53 +0200

    completing-the-task-Homework4

---
+++ Homework4/src/loop.js
@@ -0,0 +1,39 @@
+for (let i = 0; i < 10; i++) {
+    console.log (i);
+}
+
+
+let i = 0;
+
+while (i < 10) {
+    console.log (i);
+    i++;
+}
+
+
+let j = -1;
+
+do {
+    j++;
+    console.log (j);
+} while (j < 9);
+
+
+for (let i = 100; i >= 0; i -= 10) {
+    console.log (i);
+}
+
+
+let f = 100;
+
+while  (f >= 0) {
+    console.log (f);
+    f -= 10;
+}
+
+let k = 110;
+
+do {
+    k -= 10;
+    console.log (k);
+} while (k >= 10);
