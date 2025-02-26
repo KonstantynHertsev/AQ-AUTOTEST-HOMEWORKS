3789c165c6e6a4003962bdebf1dc1418191e2694
KonstantynHertsev <gktsew@gmail.com>
2025-02-26 16:50:21 +0200

    Homework9-instal-ts

---
+++ Homework9/src/index.ts
@@ -0,0 +1,16 @@
+import { IShape, Rectangle, Square } from './shapes';
+
+function displayShapeInfo(shape: IShape): void {
+    console.log(`Area: ${shape.calcArea()}`);
+    console.log(`Perimeter: ${shape.calcPerimeter()}`);
+}
+
+const rectangle = new Rectangle(4, 6);
+console.log('Rectangle:');
+displayShapeInfo(rectangle);
+
+console.log('\n-------------------\n');
+
+const square = new Square(5);
+console.log('Square:');
+displayShapeInfo(square);
